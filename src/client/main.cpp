ebe3a21afc94530acf7544ff3cbe1c31d47ce497
bernhard <bernhard.jakob.weidinger@gmail.com>
2020-04-06 12:22:44 +0200

    Add hello world to server/client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -8,12 +8,18 @@ using namespace std;
 int main() {
     //spdlog::info("Hello");
 
-    auto r = cpr::Get(cpr::Url{"localhost"},
+    auto r = cpr::Get(cpr::Url{"localhost:8080"},
                       cpr::Authentication{"user", "pass"},
                       cpr::Parameters{{"anon", "true"}, {"key", "value"}});
     //r.status_code;                  // 200
     //r.header["content-type"];       // application/json; charset=utf-8
     //r.text;                         // JSON text string
 
-    cout << r.text << endl;
+    if (r.status_code == 200) {
+        spdlog::info("Successful Request");
+        cout << r.text << endl;
+    } else {
+        spdlog::error("Request failed");
+        cerr << r.status_code << endl;
+    }
 }
