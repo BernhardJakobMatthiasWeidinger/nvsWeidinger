83ff0c547af3906e43804c3e469806985a836031
bernhard <bernhard.jakob.weidinger@gmail.com>
2020-04-06 09:31:35 +0200

    Add cpr to repo

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 #include "spdlog/spdlog.h"
+#include "cpr/cpr.h"
 
 using namespace std;
 
 int main() {
-    spdlog::info("Hello");
+    //spdlog::info("Hello");
+
+    auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                      cpr::Authentication{"user", "pass"},
+                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    r.status_code;                  // 200
+    r.header["content-type"];       // application/json; charset=utf-8
+    r.text;                         // JSON text string
 }
