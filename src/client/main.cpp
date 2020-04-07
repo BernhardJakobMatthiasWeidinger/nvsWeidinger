c37c473ba73ec7ffdf3186efe96a4434332cf369
bernhard <bernhard.jakob.weidinger@gmail.com>
2020-04-07 11:21:58 +0200

    Add hello world express server to project

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "base64.h"
+
 #include "spdlog/spdlog.h"
 #include "cpr/cpr.h"
 
@@ -9,8 +11,9 @@ int main() {
     //spdlog::info("Hello");
 
     auto r = cpr::Get(cpr::Url{"localhost:8080"},
-                      cpr::Authentication{"user", "pass"},
-                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+                      cpr::Header{{"Authorization", "Basic: HALLO"}});
+                      //cpr::Authentication{"user", "pass"});//,
+                      //cpr::Parameters{{"anon", "true"}, {"key", "value"}});
     //r.status_code;                  // 200
     //r.header["content-type"];       // application/json; charset=utf-8
     //r.text;                         // JSON text string
@@ -18,8 +21,11 @@ int main() {
     if (r.status_code == 200) {
         spdlog::info("Successful Request");
         cout << r.text << endl;
+    } else if (r.status_code == 401) {
+        spdlog::info("Not authorized, redirect to /login");
+    } else if (r.status_code == 0) {
+        spdlog::error("Server not reachable");
     } else {
-        spdlog::error("Request failed");
-        cerr << r.status_code << endl;
+        spdlog::error("Request failed with code: " + to_string(r.status_code));
     }
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,15 +1,63 @@
 #include <iostream>
 
+#include "base64.h"
 #include "restinio/all.hpp"
 
 using namespace std;
+using namespace restinio;
+
+vector<string> signedUsers;
+
+using router_t = router::express_router_t<>;
+
+bool checkAuth(request_handle_t req) {
+    if (!count(signedUsers.begin(), signedUsers.end(), 
+        req->header().get_field("Authorization"))) {
+        return false;
+    }
+    return true; 
+}
+
+auto server_handler()
+{
+	auto router = std::make_unique< router_t >();
+
+	//GET request for Homepage
+	router->http_get( "/", []( auto req, auto) {
+        if (!checkAuth(req)) {
+            return req->create_response(status_unauthorized()).done();
+        }
+
+		return req->create_response().done();
+	} );
+    
+	router->non_matched_request_handler(
+		[]( auto req ){
+			return
+				req->create_response(status_not_found() )
+					.append_header_date_field()
+					.connection_close()
+					.done();
+		} );
+
+	return router;
+}
 
 int main() {
-    restinio::run(
-        restinio::on_this_thread()
-            .port(8080)
-            .address("localhost")
-            .request_handler([](auto req) {
-                return req->create_response().set_body("Hello, World!").done();
-    }));
+    try {
+        using traits_t =
+			traits_t<
+				asio_timer_manager_t,
+				single_threaded_ostream_logger_t,
+				router_t >;
+
+        run(on_this_thread<traits_t>()
+            .port(8080).address("localhost")
+            .request_handler(server_handler()));
+            //.request_handler(handler));
+    } catch( const std::exception & ex ) {
+		cerr << "Error: " << ex.what() << endl;
+		return 1;
+	}
+   
 }
