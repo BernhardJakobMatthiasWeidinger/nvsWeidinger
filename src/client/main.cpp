e4051a7c866216eaa6b47067841f348e9a893b57
bernhard <bernhard.jakob.weidinger@gmail.com>
2020-04-11 11:10:41 +0200

    Add static class Client to client.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -8,83 +8,93 @@
 
 using namespace std;
 
-string file{"../src/client/csvFiles/clientSeil.csv"};
-string url{""};
-bool p_flag{};
+struct Client {
+    static string file;
+    static string url;
+    static bool g_flag;
 
-void processResponse(cpr::Response r, string encAuth="") {
-    if (r.status_code == 200) {
-        //if user called login
-        if (r.url.find("/login") != string::npos) {
-            spdlog::info("Successfully logged in");
+    //Credentials
+    static string username;
+    static string password;
+    static string encAuth;
+    
+    static void processResponse(cpr::Response r) {
+        if (r.status_code == 200) {
+            //if user called login
+            if (r.url.find("/login") != string::npos) {
+                spdlog::info("Successfully logged in");
+
+                cpr::Session session;
+                session.SetVerifySsl(false);
+                session.SetOption(cpr::Url{"https://localhost:1234" + Client::url});
+                session.SetOption(cpr::Header{{"Authorization", "Basic:" + Client::encAuth}});
+
+                //if user called a GET request
+                if (Client::g_flag) {
+                    processResponse(session.Get());
+                } else {
+                    session.SetOption(cpr::Multipart{{"File", cpr::File{Client::file}}});
+                    processResponse(session.Post());
+                }
+            } else {
+                //if user called a GET request
+                if (Client::g_flag) {
+                    spdlog::info("Successfully received lines from server");
+                    //print the lines of the server File
+                    cout << r.text << endl;
+                } else {
+                    spdlog::info(r.text);
+                }
+            }
+        } else if (r.status_code == 401) {
+            spdlog::warn("Not authorized, redirect to /login");
+            spdlog::info("User " + Client::username + " called GET /login");
 
             cpr::Session session;
             session.SetVerifySsl(false);
-            session.SetOption(cpr::Url{"https://localhost:1234" + url});
-            session.SetOption(cpr::Header{{"Authorization", "Basic:" + encAuth}});
-
-            //if user called a POST request
-            if (p_flag) {
-                session.SetOption(cpr::Multipart{{"File", cpr::File{file}}});
-                processResponse(session.Post(), encAuth);
-            } else {
-                processResponse(session.Get(), encAuth);
-            }
+            session.SetOption(cpr::Url{"https://localhost:1234/login"});
+            session.SetOption(cpr::Header{{"Authorization", "Basic:" + Client::encAuth}});
+            processResponse(session.Get());
+        } else if (r.status_code == 404) {
+            spdlog::error("Resource / not found");
+        } else if (r.status_code == 409) {
+            spdlog::error("Given file has errors (too many or incorrect values)");
+        } else if (r.status_code == 0) {
+            spdlog::error("Unknown error: (status_code = 0)!");
         } else {
-            //if user called a GET request
-            if (r.text.find(",") != string::npos) {
-                spdlog::info("Successfully received lines from server");
-                //print the lines of the server File
-                cout << r.text << endl;
-            } else {
-                spdlog::info(r.text);
-            }
+            spdlog::error("Request failed with code: " + to_string(r.status_code));
         }
-    } else if (r.status_code == 401) {
-        spdlog::warn("Not authorized, redirect to /login");
-
-        cpr::Session session;
-        session.SetVerifySsl(false);
-        session.SetOption(cpr::Url{"https://localhost:1234/login"});
-        session.SetOption(cpr::Header{{"Authorization", "Basic:" + encAuth}});
-
-        processResponse(session.Get(), encAuth);
-    } else if (r.status_code == 404) {
-        spdlog::error("Resource / not found");
-    } else if (r.status_code == 409) {
-        spdlog::error("Given file has errors (too many or incorrect values)");
-    } else if (r.status_code == 0) {
-        spdlog::error("Unknown error: (status_code = 0)!");
-    } else {
-        spdlog::error("Request failed with code: " + to_string(r.status_code));
     }
-}
+};
+
+string Client::file{"../src/client/csvFiles/clientSeil.csv"};
+string Client::url{};
+bool Client::g_flag{};
+
+string Client::username{};
+string Client::password{};
+string Client::encAuth{};
 
 int main(int argc, char* argv[]) {
     CLI::App app{"CPR client for the Bergrettungsbeispiel"};
 
-    //Credentials
-    string username{};
-    string password{};
-    app.add_option("--username", username, "Username to log in")->required();
-    app.add_option("--password", password, "Password to log in")->required();
+    app.add_option("--username", Client::username, "Username to log in")->required();
+    app.add_option("--password", Client::password, "Password to log in")->required();
 
     //File for post requests
-    app.add_option("--file", file, "File, which will be processed at the server");
+    app.add_option("--file", Client::file, "File, which will be processed at the server");
 
     //Method, which will be used
+    bool p_flag{};
     app.add_option_group("method", "HTTP Methode")->require_option(-1);
-
-    bool g_flag{};
     app.get_option_group("method")
-        ->add_flag("-g,--get", g_flag, "Use GET method (default)");
+        ->add_flag("-g,--get", Client::g_flag, "Use GET method (default)");
     app.get_option_group("method")
         ->add_flag("-p,--post", p_flag, "Use POST method");
 
 
-    //Object, which will be used
+    //Object, which will be processed
     app.add_option_group("object", "Inventarobjekt")->require_option(-1);
-
     bool s_flag{};
     app.get_option_group("object")
         ->add_flag("-s,--seilware", s_flag, "Seilware (default)");
@@ -98,53 +108,50 @@ int main(int argc, char* argv[]) {
     app.get_option_group("object")
         ->add_flag("-i,--immobilie", i_flag, "Immobilie");
 
+    //Number of objects, which will be posted
     int cnt{};
     app.add_option("--cnt", cnt, "Count of Inventarobjekte (default = all)");
 
     CLI11_PARSE(app, argc, argv);
     
-    string auth{username + ':' + password};
-    string encAuth = base64_encode(reinterpret_cast<const unsigned char*>(
+    //Set Credentials for HTTP Basic Authentication
+    string auth{Client::username + ':' + Client::password};
+    Client::encAuth = base64_encode(reinterpret_cast<const unsigned char*>(
         auth.c_str()), auth.length());
 
+    //Building the session
     cpr::Session session;
     session.SetVerifySsl(false);
-    session.SetOption(cpr::Header{{"Authorization", "Basic:" + encAuth}});
+    session.SetOption(cpr::Header{{"Authorization", "Basic:" + Client::encAuth}});
+
+    //Building the url
+    if (h_flag) {
+        Client::url = "/hartware";
+    } else if (f_flag) {
+        Client::url = "/fahrzeug";
+    } else if (i_flag) {
+        Client::url = "/immobilie";
+    } else {
+        Client::url = "/seilware";
+    }
 
     if (p_flag) {
-        if (h_flag) {
-            url = "/hartware";
-        } else if (f_flag) {
-            url = "/fahrzeug";
-        } else if (i_flag) {
-            url = "/immobilie";
-        } else {
-            url = "/seilware";
-        }
-
+        //if user only wants to post a specific count of objects to server
         if (cnt != 0) {
-            url += "/" + to_string(cnt);
+            Client::url += "/" + to_string(cnt);
         }
 
-        spdlog::info("User called POST " + url);
-        session.SetOption(cpr::Multipart{{"File", cpr::File{file}}});
-        session.SetOption(cpr::Url{"https://localhost:1234" + url});
+        spdlog::info("User " + Client::username + 
+            " called POST " + Client::url);
+        session.SetOption(cpr::Multipart{{"File", cpr::File{Client::file}}});
+        session.SetOption(cpr::Url{"https://localhost:1234" + Client::url});
         auto r = session.Post();
-        processResponse(r, encAuth);
+        Client::processResponse(r);
     } else {
-        if (h_flag) {
-            url = "/hartware";
-        } else if (f_flag) {
-            url = "/fahrzeug";
-        } else if (i_flag) {
-            url = "/immobilie";
-        } else {
-            url = "/seilware";
-        }
-
-        spdlog::info("User called GET " + url);
-        session.SetOption(cpr::Url{"https://localhost:1234" + url});
+        spdlog::info("User " + Client::username + 
+            " called GET " + Client::url);
+        session.SetOption(cpr::Url{"https://localhost:1234" + Client::url});
         auto r = session.Get();
-        processResponse(r, encAuth);
+        Client::processResponse(r);
     } 
 }
